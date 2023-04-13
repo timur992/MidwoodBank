1767e72b2dc83ddb7ad87b33482b554bafcc0a0f
timur r <cryptex.tim@gmail.com>
2023-04-13 19:11:31 -0400

    MB-5 Created a repository for midwood bank

---
+++ qanewyork/src/test/java/Amazon/qanewyork/AppTest.java
@@ -0,0 +1,18 @@
+package Amazon.qanewyork;
+
+import org.testng.Assert;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @org.testng.annotations.Test
+    public void shouldAnswerWithTrue()
+    {
+        Assert.assertTrue( true );
+    }
+}
---
+++ qanewyork/src/test/java/Amazon/qanewyork/FirstClass.java
@@ -0,0 +1,21 @@
+package Amazon.qanewyork;
+
+import org.testng.annotations.Test;
+
+public class FirstClass {
+
+	@Test
+	public void testOne(){
+		System.out.println("Running testOne");
+		System.out.println("First Class - Hello World One");
+
+	}
+	
+	@Test
+	public void testTwo(){
+		System.out.println("Running testTwo");
+		System.out.println("First Class - Hello World Two");
+
+	}
+
+}
